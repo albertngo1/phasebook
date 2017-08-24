20933316989d9f11c1025c61ccfe7fce95626057
Albert Ngo <albertngo1@gmail.com>
2017-08-23 21:50:41 -0400

    nav styling, additional post fixes

--- frontend/components/newsfeed/main_page.jsx
+++ frontend/components/newsfeed/main_page.jsx
@@ -12,42 +12,35 @@ class MainPage extends React.Component {
     this.props.fetchPosts();
   }
   render() {
-        const { posts, currentUser, createPost } = this.props
-        return(
-          <div className="main-page-container">
-            <header>
-              <NavBar />
-            </header>
-            <div className="main-page-content">
+    const {posts, currentUser, createPost} = this.props
+    return (
+      <div className="main-page-container">
+      <header>
+        <NavBar/>
+      </header>
+      <div className="main-page-content">
 
-              <div className="main-page-left-nav">
-              </div>
+        <div className="main-page-left-nav"></div>
 
-              <div className="main-page-newsfeed">
-                <h1>All Posts</h1>
-                <div>
-                  <PostForm currentUser={currentUser}
-                    createPost={createPost}
-                    authorId={currentUser.id} />
-                </div>
-                <ul>
-                  { posts.reverse().map( post => {
-                    return (
-                      <li key={post.id}>
-                        <p>{post.author}</p>
-                        <h1>{post.body}</h1>
-                      </li>
-                    )
-                  })}
-                </ul>
-              </div>
-              <div className="main-page-ad">
-
-              </div>
-            </div>
-
-            </div>
+        <div className="main-page-newsfeed">
+          <h1>All Posts</h1>
+          <div>
+            <PostForm currentUser={currentUser} createPost={createPost} authorId={currentUser.id}/>
           </div>
+          <ul>
+            {posts.reverse().map(post => {
+              return (
+                <li key={post.id}>
+                  <p>{post.author}</p>
+                  <h1>{post.body}</h1>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+        <div className="main-page-ad"></div>
+      </div>
+    </div>
         );
     }
 }
